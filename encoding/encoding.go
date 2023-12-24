24ed42f15600e24c373b4e4b66af4ca5956bd626
Fatum <fatumx5@mail.ru>
2023-12-24 15:38:57 +0700

    data converting

--- encoding/encoding.go
+++ encoding/encoding.go
@@ -1,7 +1,13 @@
 package encoding
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+
 	"github.com/Yandex-Practicum/final-project-encoding-go/models"
+	"gopkg.in/yaml.v3"
 )
 
 // JSONData тип для перекодирования из JSON в YAML
@@ -28,6 +34,38 @@ func (j *JSONData) Encoding() error {
 	// ниже реализуйте метод
 	// ...
 
+	jsonFileContent, err := ioutil.ReadFile(j.FileInput)
+
+	if err != nil {
+		return fmt.Errorf("read json file fail: %w", err)
+	}
+
+	if j.DockerCompose == nil {
+		j.DockerCompose = &models.DockerCompose{}
+	}
+
+	err = json.Unmarshal(jsonFileContent, j.DockerCompose)
+
+	if err != nil {
+		return fmt.Errorf("unmarshal json data fail: %w", err)
+	}
+
+	yamlFile, err := os.OpenFile(j.FileOutput, os.O_RDWR|os.O_CREATE, 0777)
+
+	if err != nil {
+		return fmt.Errorf("file open during yaml encoding fail: %w", err)
+	}
+
+	out, err := yaml.Marshal(j.DockerCompose)
+	if err != nil {
+		return fmt.Errorf("yaml encoding fail: %w", err)
+	}
+
+	_, err = yamlFile.Write(out)
+	if err != nil {
+		return fmt.Errorf("writing yaml data fail: %s", err)
+	}
+
 	return nil
 }
 
@@ -35,6 +73,37 @@ func (j *JSONData) Encoding() error {
 func (y *YAMLData) Encoding() error {
 	// Ниже реализуйте метод
 	// ...
+	yamlFileContent, err := ioutil.ReadFile(y.FileInput)
+
+	if err != nil {
+		return fmt.Errorf("read yaml file fail: %w", err)
+	}
+
+	if y.DockerCompose == nil {
+		y.DockerCompose = &models.DockerCompose{}
+	}
+
+	err = yaml.Unmarshal(yamlFileContent, y.DockerCompose)
+
+	if err != nil {
+		return fmt.Errorf("unmarshal yaml content fail: %w", err)
+	}
+
+	jsonFile, err := os.OpenFile(y.FileOutput, os.O_RDWR|os.O_CREATE, 0777)
+
+	if err != nil {
+		return fmt.Errorf("file open during json encoding fail: %w", err)
+	}
+
+	out, err := json.Marshal(y.DockerCompose)
+	if err != nil {
+		return fmt.Errorf("json encoding fail: %w", err)
+	}
+
+	_, err = jsonFile.Write(out)
+	if err != nil {
+		return fmt.Errorf("writing json data fail: %w", err)
+	}
 
 	return nil
 }
